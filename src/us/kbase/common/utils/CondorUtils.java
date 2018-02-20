af36b07c321af1d9ba7b6980b9c7513d96746dd1
Arman Mikaili <amikaili@anl.gov>
2018-02-20 12:12:59 -0600

    Change jobAd reqt to createExpressionAttribute("Requirements", "TARGET.Owner==\"root\""),

--- src/us/kbase/common/utils/CondorUtils.java
+++ src/us/kbase/common/utils/CondorUtils.java
@@ -66,7 +66,10 @@ public class CondorUtils
 			
 			// ERROR schedd log: Job id 62.0 has no Owner attribute.  Removing.
 			// TODO: Fix Requirements expression; like:
-			createExpressionAttribute("Requirements", "TARGET.Owner=='root'"),
+			
+			createExpressionAttribute("Requirements", "TARGET.Owner==\"root\""),
+			
+			// createExpressionAttribute("Requirements", "TARGET.Owner=='root'"),
 			// createExpressionAttribute("Requirements", "TARGET.Owner=='amikaili@???'"),
 			// http://research.cs.wisc.edu/htcondor/manual/v7.6/4_1Condor_s_ClassAd.html#sec:classad-reference
 			// Owner has "Policy" semantics and configuration connotation
