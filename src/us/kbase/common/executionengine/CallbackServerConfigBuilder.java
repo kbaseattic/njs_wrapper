86bae620d0eb9d229b2ff19d596f113cafb16600
bio-boris <bio-boris@users.noreply.github.com>
2018-05-18 16:42:14 -0500

    Revert "Fix refdata for subjobs"

--- src/us/kbase/common/executionengine/CallbackServerConfigBuilder.java
+++ src/us/kbase/common/executionengine/CallbackServerConfigBuilder.java
@@ -47,7 +47,6 @@ public class CallbackServerConfigBuilder {
     private URI dockerURI = null;
     final private URL callbackURL;
     final private Path workDir;
-    final private Path refDataBase;
     final private LineLogger logger;
     
     public CallbackServerConfigBuilder(
@@ -63,7 +62,6 @@ public class CallbackServerConfigBuilder {
             final URL catalogURL,
             final URL callbackURL,
             final Path workDir,
-            final Path refDataBase,
             final LineLogger logger) {
         super();
         checkNulls(kbaseEndpointURL, authServiceURL, callbackURL, workDir, logger);
@@ -80,7 +78,6 @@ public class CallbackServerConfigBuilder {
         this.catalogURL = optionalURL(catalogURL, kbaseEndpointURL, EP_CAT);
         this.callbackURL = callbackURL;
         this.workDir = workDir;
-        this.refDataBase = refDataBase;
         this.logger = logger;
     }
     
@@ -88,7 +85,6 @@ public class CallbackServerConfigBuilder {
             final Map<String, String> config,
             final URL callbackURL,
             final Path workDir,
-            final Path refDataBase,
             final LineLogger logger) {
         super();
         checkNulls(config, callbackURL, workDir, logger);
@@ -126,7 +122,6 @@ public class CallbackServerConfigBuilder {
                 true);
         this.callbackURL = callbackURL;
         this.workDir = workDir;
-        this.refDataBase = refDataBase;
         this.logger = logger;
     }
     
@@ -140,7 +135,7 @@ public class CallbackServerConfigBuilder {
         return new CallbackServerConfig(
                 kbaseEndpointURL, workspaceURL, shockURL, userJobStateURL, 
                 handleURL, srvWizURL, njswURL, authServiceURL, authAllowInsecure,
-                catalogURL, dockerURI, callbackURL, workDir, refDataBase, logger);
+                catalogURL, dockerURI, callbackURL, workDir, logger);
     }
 
 
@@ -248,12 +243,11 @@ public class CallbackServerConfigBuilder {
         final private URL callbackURL;
         final private Path workDir;
         final private LineLogger logger;
-        final public Path refDataBase;
 
         private CallbackServerConfig(URL kbaseEndpointURL, URL workspaceURL, URL shockURL, 
                 URL userJobStateURL, URL handleURL, URL srvWizURL, URL njswURL, URL authServiceURL,
                 String authAllowInsecure, URL catalogURL, URI dockerURI, URL callbackURL, 
-                Path workDir, Path refDataBase, LineLogger logger) {
+                Path workDir, LineLogger logger) {
             super();
             this.kbaseEndpointURL = kbaseEndpointURL;
             this.workspaceURL = workspaceURL;
@@ -268,7 +262,6 @@ public class CallbackServerConfigBuilder {
             this.dockerURI = dockerURI;
             this.callbackURL = callbackURL;
             this.workDir = workDir;
-            this.refDataBase = refDataBase;
             this.logger = logger;
         }
 
