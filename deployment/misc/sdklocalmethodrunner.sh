88eee647f879b845aa1bfaf1ffcce104ae09651e
bio-boris <bio-boris@users.noreply.github.com>
2018-06-21 20:05:48 -0500

    Update sdklocalmethodrunner.sh

--- deployment/misc/sdklocalmethodrunner.sh
+++ deployment/misc/sdklocalmethodrunner.sh
@@ -2,6 +2,5 @@
 JOBID=$1
 KBASE_ENDPOINT=$2
 BASE_DIR=$BASE_DIR/$JOBID
-export MINI_KB=false
 mkdir -p $BASE_DIR && cd $BASE_DIR
 java -cp "/kb/deployment/lib/NJSWrapper-all.jar" us.kbase.narrativejobservice.sdkjobs.SDKLocalMethodRunner $JOBID $KBASE_ENDPOINT > sdk_lmr.out 2> sdk_lmr.err
