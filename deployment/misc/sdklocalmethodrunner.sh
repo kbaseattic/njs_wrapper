e4d1acd383b6ecdde58aa5500c529587f3ce8e03
Boris Sadkhin <boris@bortech.us>
2018-07-18 14:31:43 -0500

    Create a multistage build

--- deployment/misc/sdklocalmethodrunner.sh
+++ deployment/misc/sdklocalmethodrunner.sh
@@ -2,18 +2,17 @@
 if [ -e /etc/clustername ] ; then
     export PATH=$PATH:$(pwd)
     export USE_SHIFTER=1
-    export REFDATA_DIR=/global/cscratch1/sd/kbaserun/refdata/ci/refdata/
+    export KBASE_RUN_DIR=/global/cscratch1/sd/kbaserun
+    export REFDATA_DIR=$KBASE_RUN_DIR/refdata/ci/refdata/
     export CALLBACK_INTERFACE=ib0
-    export SCRATCH=/global/homes/b/bsadkhin/freedom/scratch
-    echo `pwd` > /global/homes/b/bsadkhin/freedom/pwd
-    echo `ls` > /global/homes/b/bsadkhin/freedom/ls
+    export SCRATCH=/global/cscratch1/sd/kbaserun/
+    mkdir -p $SCRATCH/writeable
+    export HOME=$SCRATCH
     NJSW_JAR=`readlink -f NJSWrapper-all.jar`
     JOBID=$1
     KBASE_ENDPOINT=$2
-    BASE_DIR=/global/homes/b/bsadkhin/jobs/$JOBID
+    BASE_DIR=$KBASE_RUN_DIR/jobs/$JOBID
     mkdir -p $BASE_DIR && cd $BASE_DIR
-    echo "Jar Location = $NJSW_JAR" > jar
-    env > env
     java -cp $NJSW_JAR us.kbase.narrativejobservice.sdkjobs.SDKLocalMethodRunner $JOBID $KBASE_ENDPOINT > sdk_lmr.out 2> sdk_lmr.err
 else
     #export MINI_KB=true
