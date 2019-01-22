848426137fe8bfe4e285264b250f37fd2687421d
bio-boris <bio-boris@users.noreply.github.com>
2019-01-22 15:55:13 -0800

    Update restart_minikb.sh

--- dev_tools/restart_minikb.sh
+++ dev_tools/restart_minikb.sh
@@ -2,7 +2,6 @@
 #Add /mnt/condor in the "File Sharing tab"
 mkdir -p /mnt/condor && chmod 777 /mnt/condor
 
-
 docker-compose -f execution-engine.yml down
 docker-compose -f execution-engine.yml pull
 docker-compose -f execution-engine.yml up -d
