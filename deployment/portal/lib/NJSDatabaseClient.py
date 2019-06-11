da9de5c204085f08c0b85fd73aa8ae9d00bdf27b
Boris Sadkhin <boris@bortech.us>
2019-06-10 21:25:58 -0500

    Updated last log line

--- deployment/portal/lib/NJSDatabaseClient.py
+++ deployment/portal/lib/NJSDatabaseClient.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 import os
-import sys
-import time
 from configparser import ConfigParser
-from typing import Dict, List
+from typing import Dict
 
-from bson.objectid import ObjectId
-from pymongo import MongoClient, database, collection
-
-from .NarrativeJobServiceClient import NarrativeJobService
-from .feeds_client import feeds_client
-from .utils import send_slack_message
+from pymongo import MongoClient, database, collection, cursor
 
 
 # TODO Should I be closing mongo connections to prevent a memory leak?
@@ -21,18 +14,18 @@ from .utils import send_slack_message
 class NJSDatabaseClient:
 
     def __init__(self):
-        self.njs_db_name = self._get_config().get("NarrativeJobService", "mongodb-database")
+        self.parser = self._get_config()
+        self.njs_db_name = self.parser.get("NarrativeJobService", "mongodb-database")
         self.njs_jobs_collection_name = "exec_tasks"
         self.njs_logs_collection_name = "exec_logs"
 
     def _get_config(self) -> ConfigParser:
-        if self.parser is None:
-            self.parser = ConfigParser()
-            self.parser.read(os.environ["KB_DEPLOYMENT_CONFIG"])
-        return self.parser
+        parser = ConfigParser()
+        parser.read(os.environ["KB_DEPLOYMENT_CONFIG"])
+        return parser
 
     def _get_njs_connection(self) -> MongoClient:
-        parser = self._get_config()
+        parser = self.parser
         njs_host = parser.get("NarrativeJobService", "mongodb-host")
         njs_db = parser.get("NarrativeJobService", "mongodb-database")
         njs_user = parser.get("NarrativeJobService", "mongodb-user")
@@ -47,27 +40,61 @@ class NJSDatabaseClient:
     def _get_jobs_collection(self) -> collection:
         return self._get_jobs_database().get_collection(self.njs_jobs_collection_name)
 
-    def _get_logs_collection(self) ->collection:
+    def _get_logs_collection(self) -> collection:
         return self._get_jobs_database().get_collection(self.njs_logs_collection_name)
 
-    def get_jobs(self, job_ids, projection=None) -> List[Dict]:
-        return [self._get_jobs_collection().find({"ujs_job_id": {"$in": job_ids}}, projection=projection)]
+    def get_jobs(self, job_ids, projection=None) -> cursor:
+        return [self._get_jobs_collection().find({"ujs_job_id": {"$in": job_ids}},
+                                                 projection=projection)]
 
-    def get_logs(self, job_ids, projection=None) -> List[Dict]:
-        return [self._get_jobs_collection().find({"ujs_job_id": {"$in": job_ids}}, projection=projection)]
+    def get_logs(self, job_ids, projection=None) -> cursor:
+        return [self._get_jobs_collection().find({"ujs_job_id": {"$in": job_ids}},
+                                                 projection=projection)]
 
-    def get_job(self, job_id, projection=None) -> List[Dict]:
-        return [self._get_jobs_collection().find({"ujs_job_id": {"$eq": job_id}}, projection=projection)]
+    def get_job(self, job_id, projection=None) -> Dict:
+        return self._get_jobs_collection().find_one({"ujs_job_id": {"$eq": job_id}},
+                                                    projection=projection)
 
-    def get_log(self, job_id, projection=None) -> List[Dict]:
-        return [self._get_jobs_collection().find({"ujs_job_id": {"$eq": job_id}}, projection=projection)]
+    def get_log(self, job_id, projection=None) -> Dict:
+        return self._get_logs_collection().find_one({"ujs_job_id": {"$eq": job_id}},
+                                                    projection=projection)
 
-    def replace_log(self,job_id,replacement_document):
-        self._get_jobs_collection().replace_one(filter={"ujs_job_id": {"$eq": job_id}}, replacement=replacement_document)
+    def replace_log(self, job_id, replacement_document):
+        self._get_jobs_collection().replace_one(filter={"ujs_job_id": {"$eq": job_id}},
+                                                replacement=replacement_document)
 
+    # def update_log(self, job_id, message):
+    #     last_log_line = self.get_log(job_id)['lines'][-1]
+    #     filter = {"ujs_job_id": {"$eq": job_id}}
+    #     update =  {"$set": { "$arrayElemAt" : ["$lines", -1] } }
+    #
+    #     self._get_jobs_collection().
+    #
+    #     self._get_jobs_collection().update_one(filter=filter,
+    #                                             update=update)
 
-if __name__ == "__main__":
-    ndc = NJSDatabaseClient()
-    ujs_job_id = "565f5b6ee4b0a6527cead4a2"
-    ndc.get_job(ujs_job_id)
-    print(ndc)
+    def pop_last_log_element(self, job_id):
+        filter = {"ujs_job_id": {"$eq": job_id}}
+        popCommand = {"$pop": {"lines": 1}}
+        pop = self._get_logs_collection().update_one(filter=filter, update=popCommand)
+        print(pop.raw_result)
+
+    def push_last_log_element(self, job_id, line_object):
+        filter = {"ujs_job_id": {"$eq": job_id}}
+        pushCommand = {"$push": {"lines": line_object}}
+        push = self._get_logs_collection().update_one(filter=filter, update=pushCommand)
+        print(push.raw_result)
+
+    #It would be great to use
+    #           - `array_filters` (optional): A list of filters specifying which
+    #        array elements an update should apply. Requires MongoDB 3.6+.
+
+    def update_last_log_line(self, job_id, message):
+        last_log_line = self.get_log(job_id)['lines'][-1]
+        last_log_line["line"] += f" {message}\n "
+        last_log_line["is_error"] = True
+
+        self.get_log(job_id)
+        self.pop_last_log_element(job_id)
+        self.push_last_log_element(job_id, last_log_line)
+        self.get_log(job_id)
