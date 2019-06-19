2c7c66280292413b16fbf7f78add7acccbdd4956
Boris Sadkhin <boris@bortech.us>
2019-06-19 17:06:45 -0500

    Added logging, small modifications

--- deployment/portal/lib/ExecutionEngineJobs.py
+++ deployment/portal/lib/ExecutionEngineJobs.py
@@ -1,52 +1,60 @@
-from typing import List,Dict
+import logging
+from typing import Dict
+
+from pymongo import collection as pymongo_collection
+
+from HTCondorWrapper import HTCondorWrapper
 from lib.clients.NJSDatabaseClient import NJSDatabaseClient
 from lib.clients.UJSDatabaseClient import UJSDatabaseClient
 
+
 class ExecutionEngineJobs:
-    jsc = {
-        "0": "Unexepanded",
-        1: "Idle",
-        2: "Running",
-        3: "Removed",
-        4: "Completed",
-        5: "Held",
-        6: "Submission_err",
-        -1: "Not found in condor",
-    }
 
-
-    def _find_incomplete_jobs(self) -> List[Dict]:
+    def _find_incomplete_ujs_jobs(self) -> Dict[str, pymongo_collection]:
         # TODO USE PROJECTION
         """
 
         :return:
         """
-        if self.incomplete_jobs is None:
-            jobstate = self.ujs_db.get_jobs_collection()
-            self.incomplete_jobs = list(jobstate.find({"complete": {"$ne": True}}))
-        return self.incomplete_jobs
+
+        jobstate = self.ujs_db.get_jobs_collection()
+        incomplete_jobs = list(jobstate.find({"complete": {"$ne": True}}))
+        ij = {}
+        for job in incomplete_jobs:
+            ij[str(job['_id'])] = job
+        return ij
 
     def __init__(self):
         self.incomplete_jobs = None
         self.njs_db = NJSDatabaseClient()
         self.ujs_db = UJSDatabaseClient()
 
-
-
     def log_incomplete_jobs(self):
-        self.incomplete_jobs = self._find_incomplete_jobs()
-        for job in self.incomplete_jobs:
-            print(job)
+        """
+        This function is for testing purposes
+        :return:
+        """
+        incomplete_ujs_jobs = self._find_incomplete_ujs_jobs()
+        condor_jobs = HTCondorWrapper.get_condor_q_jobs()
+        icc = len(incomplete_ujs_jobs.keys())
 
-        # for job in incomplete_jobs():
-        #     writeLogFile
+        incomplete_jobs = {}
+
+        # Store incomplete jobs
+        for job_id in incomplete_jobs.keys():
+            condor_job = incomplete_jobs[job_id]
+            if job_id in condor_jobs:
+                if HTCondorWrapper.job_will_complete(condor_job):
+                    continue
+                incomplete_jobs[job_id] = condor_job
+
+        logging.info(
+            f"Found {icc} incomplete jobs. Of these, {len(incomplete_jobs.keys())} are actually incomplete")
+
+
 
     def find_incomplete_jobs(self):
         self.log_incomplete_jobs()
 
-
     def purge_incomplete_jobs(self):
         pass
-
-
-
